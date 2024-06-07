6cf42fbd399c994bd7080325cdbece0d9e21b9bc
aidanaidanaidanaidanaidannaidan <168770870+aidanaidanaidanaidanaidannaidan@users.noreply.github.com>
2024-06-07 09:37:57 +0100

    Update exercise_11.py

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,2 +1,10 @@
 # Your solution to Exercise 11
 
+year = int(input("Enter a year: "))
+if 1900 ≤ year and year ≤ 3000:
+  if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print("Leap")
+  else:
+    print("Common")
+else:
+  print("Invalid input")
