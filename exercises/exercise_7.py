fea10b166591176eeb8574b618d6762d632636f4
aidanaidanaidanaidanaidannaidan <168770870+aidanaidanaidanaidanaidannaidan@users.noreply.github.com>
2024-06-07 09:36:36 +0100

    Update exercise_7.py

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,2 +1,22 @@
 # Your solution to Exercise 7
 
+num1 = float(input("enter first num: "))
+num2 = float(input("enter second num: "))
+operation = input("enter operation: ")
+if operation == "+":
+  print(num1 + num2)
+elif operation == "-":
+  print(num1 - num2)
+elif operation == "*":
+  print(num1 * num2)
+elif operation == "/":
+  if num2 == 0:
+    print("Error: division by zero")
+  else:
+    print(num1 / num2)
+elif operation == "mod":
+  print(num1 % num2)
+elif operation == "pow":
+  print(num1 ** num2)
+else:
+  print("invalid operation")
